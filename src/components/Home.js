8b92223354b189c4c69314a62802c32516ab6bb9
Brayan Estiven Diaz Alvarez <brayanestive18@gmail.com>
2020-03-27 16:45:22 -0500

    Fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,9 @@ import React, { Component } from "react";
 // import Player3 from './Player3'
 // import Player4 from "./Player4";
 // import Player5 from "./Player5";
-// import Player6 from "./Player6";
+import Player6 from "./Player6";
 // import Player7 from "./Player7";
-import Player8 from "./Player8";
+// import Player8 from "./Player8";
 // import ShakaPlayer from 'shaka-player-react';
 import "../App.css";
 
@@ -27,9 +27,9 @@ class Home extends Component {
                 {/* <Player3/> */}
                 {/* <Player4 /> */}
                 {/* <Player5 /> */}
-                {/* <Player6 /> */}
+                <Player6 />
                 {/* <Player7 /> */}
-                <Player8 />
+                {/* <Player8 /> */}
                 {/* <ShakaPlayer autoPlay src="http://hlslive.lcdn.une.net.co/v1/AUTH_HLSLIVE/TANT/tu1_manifest.m3u8" /> */}
             </div>
         );
