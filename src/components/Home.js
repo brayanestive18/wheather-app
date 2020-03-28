0369cd6dd1d7333cefdc7d50b7e67470afe4c21a
Brayan Estiven Diaz Alvarez <brayanestive18@gmail.com>
2020-03-28 17:46:21 -0500

    props

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,10 @@ import "../App.css";
 
 class Home extends Component {
     render() {
+
+        var manifestUri = new URLSearchParams(window.location.search).get("url");
+        console.log(manifestUri);
+
         return (
             <div>
                 <header className="App-header">
@@ -27,7 +31,7 @@ class Home extends Component {
                 {/* <Player3/> */}
                 {/* <Player4 /> */}
                 {/* <Player5 /> */}
-                <Player6 />
+                <Player6 manifestUri={manifestUri} />
                 {/* <Player7 /> */}
                 {/* <Player8 /> */}
                 {/* <ShakaPlayer autoPlay src="http://hlslive.lcdn.une.net.co/v1/AUTH_HLSLIVE/TANT/tu1_manifest.m3u8" /> */}
