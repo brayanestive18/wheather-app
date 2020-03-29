2678cd0f2954a0986b81284d327a10327ff30145
Brayan Estiven Diaz Alvarez <brayanestive18@gmail.com>
2020-03-28 19:21:28 -0500

    drm

--- src/components/Player6.js
+++ src/components/Player6.js
@@ -27,17 +27,17 @@ class Player6 extends Component {
     console.log(this.player.getConfiguration());
 
     // .load(this.props.playURL)
-    // var manifestUri =
+    var manifestUri =
     // "https://livesim.dashif.org/livesim/testpic_2s/Manifest.mpd";
     // "https://bitmovin-a.akamaihd.net/content/art-of-motion_drm/mpds/11331.mpd";
     // "https://content.jwplatform.com/manifests/yp34SRmf.m3u8";
     // "http://hlslive.lcdn.une.net.co/v1/AUTH_HLSLIVE/TANT/tu1_manifest.m3u8";
     // "https://storage.googleapis.com/shaka-live-assets/player-source.m3u8";
     // "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
-    // "https://content.uplynk.com/channel/d71e847cb6d1424a8288996384a8ebc1.mpd?oid=d09b16c953aa40c98dd8c513526aca5a&ad=espn_latamlive_es&ad.vast3=1&ad.vid=d87aee04-c5a2-4999-84e6-d73d17cb8a87&ad.tfcd=0&ad.adUnit=/espn2_colombia/espnplay/null&ad.pp=espn-core-ssai&ad.vip=179.13.112.247&ad.ppid=&ad.description_url=null&ss_req=1&ad.ssss=vdms&ad.hl=es&ad.npa=0&ad.msid=null&ad.an=espnapp&ad.extcalls=liveconnect&ad.cust_params=chan=espn2_colombia%26authp=null%26nlsnAppID=null%26nlsnDevGrp=devgrp%2CUNWN%26vdm=live%26devOS=null%26devType=null%26plt=null%26swid=%26unid=null%26isDnt=0%26isAutoplay=0%26isMute=0%26vps=null%26stp=vdms%26lang=es%26ip=179.13.112.247&eid=n177a&ct=c&tc=1&exp=1585430313&rn=1377491800&srs=templatetimeline&drm_policy_name=espn_live_drm&geo=n177&sig=6d94163144bc9e58479f4077f3ab0595c31f883f313fd9c260c5a2807786bb7f";
+      "https://media.axprod.net/TestVectors/v7-MultiDRM-MultiKey/Manifest_1080p.mpd";
 
     this.player
-      .load(this.props.manifestUri)
+      .load(manifestUri)
       .then(() => {
         console.log("Ready");
       })
@@ -47,7 +47,7 @@ class Player6 extends Component {
       drm: {
         servers: {
           "com.widevine.alpha": "https://widevine-proxy.appspot.com/proxy",
-          "com.microsoft.playready": "https://content.uplynk.com/pr"
+          "com.microsoft.playready": "https://drm-playready-licensing.axtest.net/AcquireLicense"
           // "https://playready.directtaps.net/pr/svc/rightsmanager.asmx?PlayRight=1&ContentKey=EAtsIJQPd5pFiRUrV9Layw=="
         }
       }
